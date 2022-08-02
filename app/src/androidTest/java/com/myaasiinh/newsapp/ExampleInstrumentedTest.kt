6d8a9f594e8c2b5bf8e254768eeb4bdf4ff32a44
myaasiinh <myaasiinh@gmail.com>
2022-08-03 00:12:30 +0700

    refactor colors.xml

--- app/src/androidTest/java/com/myaasiinh/newsapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/myaasiinh/newsapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.android.newsapp", appContext.packageName)
+        assertEquals("com.myaasiinh.newsapp", appContext.packageName)
     }
 }
